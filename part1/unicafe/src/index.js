40857416bb6a7d878369673c475147af5f741590
Leopra <Leopica1>
2021-01-31 19:36:56 +0200

    exercise 1.8

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -12,6 +12,33 @@ const Average = (props) => {
 const Percent = (props) => {
   return (<div>positive {(props.good) / (props.good + props.neutral + props.bad)} </div>)
 }
+
+const Statistics = (props) => {
+  return (<div>
+    <div><h1>give feedback</h1></div>
+
+    <div>
+      <button onClick={() => props.setGood(props.good + 1)}>
+        good
+    </button>
+      <button onClick={() => props.setNeutral(props.neutral + 1)}>
+        neutral
+    </button>
+      <button onClick={() => props.setBad(props.bad + 1)}>
+        bad
+    </button>
+    </div>
+    <div><h1>statistics</h1></div>
+    <div>good {props.good}</div>
+    <div>neutral {props.neutral}</div>
+    <div>bad {props.bad}</div>
+    <div>
+      <All good={props.good} neutral={props.neutral} bad={props.bad}></All>
+      <Average good={props.good} neutral={props.neutral} bad={props.bad}></Average>
+      <Percent good={props.good} neutral={props.neutral} bad={props.bad}></Percent>
+    </div>
+  </div>)
+}
 const App = () => {
   // tallenna napit omaan tilaansa
   const [good, setGood] = useState(0)
@@ -19,31 +46,7 @@ const App = () => {
   const [bad, setBad] = useState(0)
 
   return (
-    <div>
-      <div><h1>give feedback</h1></div>
-
-      <div>
-        <button onClick={() => setGood(good + 1)}>
-          good
-      </button>
-        <button onClick={() => setNeutral(neutral + 1)}>
-          neutral
-      </button>
-        <button onClick={() => setBad(bad + 1)}>
-          bad
-      </button>
-      </div>
-      <div><h1>statistics</h1></div>
-      <div>good {good}</div>
-      <div>neutral {neutral}</div>
-      <div>bad {bad}</div>
-      <div>
-        <All good={good} neutral={neutral} bad={bad}></All>
-        <Average good={good} neutral={neutral} bad={bad}></Average>
-        <Percent good={good} neutral={neutral} bad={bad}></Percent>
-      </div>
-    </div>
-
+    <Statistics setGood={setGood} setNeutral={setNeutral} setBad={setBad} good={good} neutral={neutral} bad={bad}></Statistics>
   )
 }
 
