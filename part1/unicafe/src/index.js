6035b4752d57898ef3bc79780df3ca1ce5ca7e5f
Leopra <Leopica1>
2021-01-31 18:42:34 +0200

    exercise 1.6

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <div><h1>give feedback</h1></div>
+
+      <div>
+        <button onClick={() => setGood(good + 1)}>
+          good
+      </button>
+        <button onClick={() => setNeutral(neutral + 1)}>
+          neutral
+      </button>
+        <button onClick={() => setBad(bad + 1)}>
+          bad
+      </button>
+      </div>
+      <div><h1>statistics</h1></div>
+      <div>good {good}</div>
+      <div>neutral {neutral}</div>
+      <div>bad {bad}</div>
+    </div>
+
+  )
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
