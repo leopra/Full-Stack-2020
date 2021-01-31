f2a2f155c2bd44885c08ae2426ae3ec30d6697da
Leopra <Leopica1>
2021-01-31 20:38:44 +0200

    1.11

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -2,19 +2,19 @@ import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
 const All = (props) => {
-  return (<tr><td>all</td> <td>{props.good + props.neutral + props.bad}</td></tr>)
+  return (<tr><td>all</td><td>{props.good + props.neutral + props.bad}</td></tr>)
 }
 
 const Average = (props) => {
-  return (<tr><td>average</td> <td>{(props.good * 1 + props.neutral * 0 + props.bad * -1) / (props.good + props.neutral + props.bad)}</td> </tr>)
+  return (<tr><td>average</td><td>{(props.good * 1 + props.neutral * 0 + props.bad * -1) / (props.good + props.neutral + props.bad)}</td></tr>)
 }
 
 const Percent = (props) => {
-  return (<tr><td>positive</td> <td>{(props.good) / (props.good + props.neutral + props.bad)}</td> </tr>)
+  return (<tr><td>positive</td><td>{(props.good) / (props.good + props.neutral + props.bad)}</td></tr>)
 }
 
 const Statistic = (props) => {
-  return (<tr><td>{props.text}</td> <td>{props.value}</td></tr>)
+  return (<tr><td>{props.text}</td><td>{props.value}</td></tr>)
 }
 const Statistics = (props) => {
   if ((props.good + props.neutral + props.bad) !== 0) {
@@ -22,14 +22,14 @@ const Statistics = (props) => {
     return (<div>
       <div><h1>statistics</h1></div>
       <table>
-        <Statistic text='good' value={props.good}></Statistic>
-        <Statistic text='neutral' value={props.neutral}></Statistic>
-        <Statistic text='bad' value={props.bad}></Statistic>
-
-        <All good={props.good} neutral={props.neutral} bad={props.bad}></All>
-        <Average good={props.good} neutral={props.neutral} bad={props.bad}></Average>
-        <Percent good={props.good} neutral={props.neutral} bad={props.bad}></Percent>
-
+        <tbody>
+          <Statistic text='good' value={props.good}></Statistic>
+          <Statistic text='neutral' value={props.neutral}></Statistic>
+          <Statistic text='bad' value={props.bad}></Statistic>
+          <All good={props.good} neutral={props.neutral} bad={props.bad}></All>
+          <Average good={props.good} neutral={props.neutral} bad={props.bad}></Average>
+          <Percent good={props.good} neutral={props.neutral} bad={props.bad}></Percent>
+        </tbody>
       </table>
     </div>)
   }
