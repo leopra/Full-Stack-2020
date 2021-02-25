11b137a338d17b8808e17a0fb8b5678df09b3255
Leopra <Leopica1>
2021-02-25 20:12:50 +0200

    4.18-4.19

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -6,6 +6,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
+const loginRouter = require('./controllers/login')
 const usersRouter = require('./controllers/users')
 
 
@@ -16,6 +17,8 @@ const blogRouter = require('./controllers/blog')
 
 app.use('/api/blogs', blogRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
+
 
 const mongoUrl = config.MONGODB_URI
 logger.info('connecting to', mongoUrl)
--- part4/bloglist/controllers/blog.js
+++ part4/bloglist/controllers/blog.js
@@ -1,6 +1,15 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+    return authorization.substring(7)
+  }
+  return null
+}
 
 //TODO why populate user is singular, blogs plural?
 blogRouter.get('/', async (request, response) => {
@@ -13,9 +22,16 @@ blogRouter.get('/', async (request, response) => {
 
 blogRouter.post('/', async (request, response) => {
   const body = request.body
+  const token = getTokenFrom(request)
+
+  const decodedToken = jwt.verify(token, process.env.SECRET)
+
+  if (!token || !decodedToken.id) {
+    return response.status(401).json({ error: 'token missing or invalid' })
+  }
 
   if (!body.title || !body.url) {
-    response.status(400).json({ "result": "Bad Request" })
+    return response.status(400).json({ "result": "Bad Request" })
   }
 
   const user = await User.findOne()
