53bf43360f5341c6b9bbe7b6028cbdbd1c0e5f28
Leopra <Leopica1>
2021-02-25 16:23:10 +0200

    4.15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -6,11 +6,16 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
 
+const usersRouter = require('./controllers/users')
+
+
 app.use(cors())
 app.use(express.json())
 
 const blogRouter = require('./controllers/blog')
+
 app.use('/api/blogs', blogRouter)
+app.use('/api/users', usersRouter)
 
 const mongoUrl = config.MONGODB_URI
 logger.info('connecting to', mongoUrl)
