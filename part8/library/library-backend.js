2d66cbcb9ba5705cd5e34bf70904f7526e12d65e
Leopra <Leopica1>
2021-04-17 12:48:26 +0300

    8.2-8.3

--- part8/library/library-backend.js
+++ part8/library/library-backend.js
@@ -84,16 +84,33 @@ let books = [
 ]
 
 const typeDefs = gql`
+  type Book {
+    title: String!
+    published: Int
+    author: String!
+    id: ID!
+    genres: [String]
+  }
+
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+  }
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books,
+    allAuthors: () => authors
   }
 }
 
@@ -101,6 +118,9 @@ const server = new ApolloServer({
   typeDefs,
   resolvers,
 })
+
+
+
 
 server.listen().then(({ url }) => {
   console.log(`Server ready at ${url}`)
