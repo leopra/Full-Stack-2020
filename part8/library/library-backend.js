a3858541eb27ad1e2527f0ea59cba28989bf8cbe
Leopra <Leopica1>
2021-04-17 13:44:29 +0300

    8.5

--- part8/library/library-backend.js
+++ part8/library/library-backend.js
@@ -101,7 +101,7 @@ const typeDefs = gql`
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks(author: String): [Book!]!
+    allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
 `
@@ -110,7 +110,12 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: (root, args) => books.filter(b => b.author === args.author),
+    allBooks: (root, args) => {
+      if (args.author) { return books.filter(b => b.author === args.author)}
+      else if (args.genre) {
+        return books.filter(b => b.genres.includes(args.genre)) }
+      else { return books}
+      },
     allAuthors: () => authors
   }
 }
