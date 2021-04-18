523ff319dce745e6835784467cd10a4bd561ca7e
Leopra <Leopica1>
2021-04-18 14:35:37 +0300

    8.19

--- part8/library-frontend/src/components/Books.js
+++ part8/library-frontend/src/components/Books.js
@@ -1,14 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ALL_BOOKS } from "../queries"
 import { useQuery } from '@apollo/client'
+import Select from "react-select"
 
 const Books = (props) => {
+  const [genre, setGenre] = useState("")
   const result = useQuery(ALL_BOOKS)
+
 
   if (!props.show) {
     return null
   }
-  const books = result.data.allBooks
+  let books = result.data.allBooks
+  books = genre ? result.data.allBooks.filter((bok) => bok.genres.includes(genre)) : books
+
+  const handleChange = (data) => {
+    const label = data?.label;
+    setGenre(label)
+  }
+
+  let genres = result.data?.allBooks.flatMap(book => book.genres)
+
+  const opts = Array.from(new Set(genres)).map(op => {
+    return {
+      value: op,
+      label: op
+    }
+  }
+  )
 
   return (
     <div>
@@ -34,6 +53,11 @@ const Books = (props) => {
           )}
         </tbody>
       </table>
+      <Select
+        options={opts}
+        onChange={handleChange}
+        value={genre ? { label: genre, value: genre?.toLowerCase() } : null}
+      />
     </div>
   )
 }
